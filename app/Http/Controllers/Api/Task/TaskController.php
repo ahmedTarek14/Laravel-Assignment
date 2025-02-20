ae61190019691c4c2d99211f89ad4153882c9f94
ahmedTarek14 <ahmedtarek.am@gmail.com>
2025-02-20 22:17:18 +0200

    all tasks api

--- app/Http/Controllers/Api/Task/TaskController.php
+++ app/Http/Controllers/Api/Task/TaskController.php
@@ -12,12 +12,10 @@ class TaskController extends Controller
 {
     public function index()
     {
-        $user_id = sanctum()->id();
-        $tasks = Task::where('user_id', $user_id)->orderByDesc('id')->get();
-
-        $data = TaskResource::collection($tasks)->response()->getData(true);
-
         try {
+            $user_id = sanctum()->id();
+            $tasks = Task::where('user_id', $user_id)->orderByDesc('id')->get();
+            $data = TaskResource::collection($tasks)->response()->getData(true);
             return api_response_success($data);
         } catch (\Throwable $th) {
             return api_response_error();
@@ -26,16 +24,33 @@ class TaskController extends Controller
 
     public function store(TaskRequest $request)
     {
-        $request['user_id'] = sanctum()->id();
-
-        $task = Task::create($request->all());
-
         try {
-            $data = new TaskResource($task);
-
-            return api_response_success($data);
+            $request['user_id'] = sanctum()->id();
+            $task = Task::create($request->all());
+            return api_response_success(['message' => 'Task created successfully.']);
         } catch (\Throwable $th) {
             // dd($th->getMessage());
+            return api_response_error();
+        }
+    }
+
+
+    public function update(TaskRequest $request, Task $task)
+    {
+        try {
+            $task->update($request->all());
+            return api_response_success(['message' => 'Task updated successfully.']);
+        } catch (\Throwable $th) {
+            return api_response_error();
+        }
+    }
+
+    public function destroy(Task $task)
+    {
+        try {
+            $task->delete();
+            return api_response_success(['message' => 'Task deleted successfully.']);
+        } catch (\Throwable $th) {
             return api_response_error();
         }
     }
